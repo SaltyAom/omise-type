24ffddd27c6de6bc94a87001e1fb4614a40dce20
SaltyAom <aomkirby123@gmail.com>
2021-04-19 12:17:21 +0700

    :tada: feat - add build

--- src/index.ts
+++ src/index.ts
@@ -13,17 +13,17 @@ import OmiseCard from './lib/omiseCard'
 declare global {
 	interface Window {
 		/**
-		 * You can use the following methods on `OmiseCard` to customize the appearance and behavior of your form.
-		 * 
-		 * @see https://www.omise.co/omise-js#omisecard-methods
-		 */
-		Omise: Omise
-		/**
 		 * You can use the following methods on `Omise` to create a one-time-use token or source.
 		 * 
 		 * @see https://www.omise.co/omise-js#omise-methods
 		 */
-		OmiseCard: OmiseCard
+		 Omise: Omise
+		/**
+		 * You can use the following methods on `OmiseCard` to customize the appearance and behavior of your form.
+		 * 
+		 * @see https://www.omise.co/omise-js#omisecard-methods
+		 */
+		 OmiseCard: OmiseCard
 	}
 	const Omise: Omise
 	const OmiseCard: OmiseCard
